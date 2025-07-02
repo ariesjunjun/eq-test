37250f0e5dcabae12ec6dddd0281b1d3f15a51d6
ariesjunjun <aoarashihazuki@gmail.com>
2025-07-02 15:11:54 +0900

    vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/eq-test/', // ★ここはリポジトリ名に合わせる！
-})
+  base: '/eq-test/',
+});
